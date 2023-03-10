1aea26a7310696e388a88a4ceab98e4a932d7d01
patrickkorhonen <patrick.korhonen2@gmail.com>
2023-03-10 13:01:37 +0200

    updated database

--- database/database.js
+++ database/database.js
@@ -1,5 +1,10 @@
-import {postgres} from "../deps.js";
+import { postgres } from "../deps.js";
 
-const sql = postgres({});
+let sql;
+if (Deno.env.get("DATABASE_URL")) {
+  sql = postgres(Deno.env.get("DATABASE_URL"));
+} else {
+  sql = postgres({});
+}
 
 export { sql };
